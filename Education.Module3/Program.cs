179965730a626b53c58dd0a81e90e900a5de00f6
KokProg <timoorka616@gmail.com>
2022-03-07 00:27:22 +0500

    Добавьте файлы проекта.

---
+++ Education.Module3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Education.Module3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Timur";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
