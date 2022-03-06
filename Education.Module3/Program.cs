308441d7bfba57ebfbe7b87ed5cddd39811b318d
KokProg <timoorka616@gmail.com>
2022-03-07 01:08:37 +0500

    Переменные, константы, литералы

--- Education.Module3/Program.cs
+++ Education.Module3/Program.cs
@@ -6,10 +6,19 @@ namespace Education.Module3
     {
         static void Main(string[] args)
         {
-            string myName= "Timur";
-           
+            const string myName= "Timur";
             Console.WriteLine(myName);
-
+            Console.WriteLine("\t Привет, Мир");
+            Console.WriteLine("\t Мне 31 год");
+            Console.WriteLine("\t My mane is \n Timur");
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            Console.WriteLine(5);
+            Console.WriteLine(0x0A);
+            Console.WriteLine(0b11);
+            Console.WriteLine(5.5);
             Console.ReadKey();
         }
     }
