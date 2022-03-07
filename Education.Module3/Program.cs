52270827fb9e55d2ed4bdc8c5ad461273d67eb61
KokProg <timoorka616@gmail.com>
2022-03-07 16:18:18 +0500

    Ввод и вывод в консоль

--- Education.Module3/Program.cs
+++ Education.Module3/Program.cs
@@ -5,8 +5,9 @@ namespace Education.Module3
     class Program
     {
         static void Main(string[] args)
+
         {
-            const string myName= "Timur";
+            const string myName = "Timur";
             Console.WriteLine(myName);
             Console.WriteLine("\t Привет, Мир");
             Console.WriteLine("\t Мне 31 год");
@@ -19,10 +20,10 @@ namespace Education.Module3
             Console.WriteLine(0x0A);
             Console.WriteLine(0b11);
             Console.WriteLine(5.5);
-            Console.ReadKey();
-            
-            string name = "Timur";
-            byte age = 31;
+      
+            string name = "Jane";
+            byte age = 27;
+            string favcolor = "black";
             bool HaveIApet = true;
             double MyShoeSize = 43.5;
 
@@ -33,7 +34,25 @@ namespace Education.Module3
 
             Console.WriteLine("IntMin {0} ", int.MinValue);
             Console.WriteLine("IntMax {0} ", int.MaxValue);
+            Console.WriteLine("\t Привет, \n мир!");
 
+            byte Age = 31;
+            Console.WriteLine($"Мой возраст {Age}");
+
+            Console.WriteLine("{0}\n{1}\n{2}", name, age,favcolor);
+
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+
+            Console.ReadKey();
         }
+           enum Semaphore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green=300
+        }
+
+        
     }
 }
