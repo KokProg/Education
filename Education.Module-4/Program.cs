cbde16cf42c27e0d07459a75383d1ca634061cfe
KokProg <timoorka616@gmail.com>
2022-03-10 22:51:03 +0500

    Создан проект Education.Module-4

---
+++ Education.Module-4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Education.Module_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
