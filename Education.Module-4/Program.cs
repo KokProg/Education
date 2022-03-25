8e443b3c36428d473335da4d096ca7add0c2662c
KokProg <timoorka616@gmail.com>
2022-03-25 13:29:15 +0500

    Задание 4.1.17

--- Education.Module-4/Program.cs
+++ Education.Module-4/Program.cs
@@ -39,9 +39,88 @@ namespace Education.Module_4
             bool c = (a < b) || (x > y);
             Console.WriteLine(c);
 
+            var f = 6;
+            var h = 11;
+            if (f == h)
+            {
+                Console.WriteLine("Условие истино");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+
+                if (h < 10)
+                {
+                    Console.WriteLine("Значение h = {0} меньше 10", h);
+                }
+                else
+                {
+                    Console.WriteLine("Значение h = {0} больше 10", h);
+                }
+            }
+                if (f == h)
+                {
+                    Console.WriteLine("Условие истино");
+                }
+                else if(h<10)
+                {
+                    Console.WriteLine("Значение h = {0} меньше 10", h);
+                }
+                else
+                {
+                    Console.WriteLine("Значение h = {0} больше 10", h);
+                }
+            
+                if (f == h && b > 1)
+            {
+                Console.WriteLine("Условие истино");
+            }
+                else if (b > 10 || b == 7)
+            {
+                Console.WriteLine("Значение h = {0} больше 10 или равно 7", h);
+            }
+            else 
+            {
+                Console.WriteLine("Значение h = {0}", h);
+            }
+
+            var g = f != h ? f + h : h;
+
+            Console.WriteLine(g);
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+
+            {
+               Console.BackgroundColor = ConsoleColor.DarkRed;
+               Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+
+            }
+
+            else if (color == "green")
+
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+            Console.ReadKey();
 
 
-                Console.ReadKey();
         }
     }
 }
